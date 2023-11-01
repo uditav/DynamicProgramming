cd6df1e49cf7cd32f0f61f9ce5bbfe157351dfc6
uditav <100796411+uditav@users.noreply.github.com>
2023-11-01 18:51:29 +0530

    Update 14SumSubsetEqualToTarget.cpp

--- 14SumSubsetEqualToTarget.cpp
+++ 14SumSubsetEqualToTarget.cpp
@@ -30,14 +30,62 @@ bool f(int n, int k, vector<int> &arr, vector<vector<int>>& dp) {
 }
 
 bool subsetSumToK(int n, int k, vector<int> &arr){
-    vector<vector<int>> dp(10000,vector<int>(1000,-1));
+    vector<vector<int>> dp(n,vector<int>(k+1,-1));
     return f(n-1,k,arr,dp);
 }
 TC -> O(N * k)
 SC -> O(N * k) + O(N)
 
+//Tabulation
 
+bool f(int n, int k, vector<int>& arr, vector<vector<bool>>& dp){
+    for(int i=0;i<=n;i++) dp[i][0]=true;
+    dp[0][arr[0]]=true;
 
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=k;j++){
+           bool nottake=dp[i-1][j];
+           bool take=false;
+           if (arr[i] <= j) {
+             take = dp[i - 1][j - arr[i]];
+           }
+           dp[i][j] = take | nottake;
+        }
+    }
+    return dp[n][k];
+}
 
+bool subsetSumToK(int n, int k, vector<int> &arr) {
+    vector<vector<bool>> dp(n,vector<bool>(k+1,0));
+    return f(n-1,k,arr,dp);
+}
 
+TC -> O(N * k)
+SC -> O(N * k)
+
+//Space optimization
+bool f(int n, int k, vector<int>& arr){
+    vector<bool> curr(k+1,0), prev(k+1,0);
+    prev[0]=curr[0]=true;
+    prev[arr[0]]=true;
+
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=k;j++){
+           bool nottake=prev[j];
+           bool take=false;
+           if (arr[i] <= j) {
+             take = prev[j - arr[i]];
+           }
+           curr[j] = take | nottake;
+        }
+        prev=curr;
+    }
+    return prev[k];
+}
+
+bool subsetSumToK(int n, int k, vector<int> &arr) {
+    return f(n-1,k,arr);
+}
   
+TC -> O(N * k)
+SC -> O(k)
